b5658271ec08899c29d05400045fb9827df15e67
Giovanna Barbosa <96540576+Giozsv@users.noreply.github.com>
2022-02-17 14:48:31 -0300

    Create Retangulo.cs

---
+++ Modulo/Retangulo.cs
@@ -0,0 +1,34 @@
+namespace ExemploPOO.Modulo
+{
+    public class Retangulo
+    {
+        private double comprimento;
+        private double largura;
+        private bool valido;
+        public void DefinirMedidas(double comprimento, double largura)
+        {
+            if (comprimento > 0 && largura > 0)
+            {
+                this.comprimento = comprimento;
+                this.largura = largura;
+                valido = true;
+            }
+            else
+            {
+                System.Console.WriteLine("Valores INválidos");
+            }    
+        }
+        public double ObterArea()
+        {
+            if(valido)
+            {
+                return comprimento * largura;
+            }
+            else
+            {
+                System.Console.WriteLine("Preencha valores validos");
+                return 0;
+            }
+        }
+    }
+}
