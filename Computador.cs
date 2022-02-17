85d623f9e68ba609b3be7a1720f45c38221f3767
Giovanna Barbosa <96540576+Giozsv@users.noreply.github.com>
2022-02-17 14:41:49 -0300

    Create Computador.cs

---
+++ Computador.cs
@@ -0,0 +1,10 @@
+namespace ExemploPOO.Modulo
+{
+    public class Computador : System.Object //Herança de maneira implicita\\
+    {
+        public override string ToString()
+        {
+            return "Sou uma classe computador";
+        }
+    }
+}
